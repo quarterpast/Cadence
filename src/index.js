c05b745b404ceb72196cfd23269ebd7e8fc5566d
Matt Brennan <mattyb1000@gmail.com>
2014-11-12 21:21:52 +0000

    pattern match

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ var esc = require('escodegen');
 var ast = require('ast-types');
 var b   = ast.builders;
 
-function compile(tree) {
-	return Array.isArray(tree) ? tree[0] === 'quote' ? b.arrayExpression(tree.slice(1).map(b.literal))
-	                                                 : b.callExpression(compile(tree[0]), tree.slice(1).map(compile))
-	     : tree instanceof String ? b.literal(String(tree))
-	     :                     b.identifier(tree);
+function compile {
+	['quote', ...rest]       => b.arrayExpression(rest.map(b.literal)),
+	[callee,  ...rest]       => b.callExpression(compile(callee), rest.map(compile)),
+	x if x instanceof String => b.literal(String(x)),
+	x                        => b.identifier(x)
 }
 
 console.log(esc.generate(compile(sex("(a b '(c d) \"frsnt\")"))));
