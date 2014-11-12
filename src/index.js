d69d9dbadfafa34a381d649b3891ad18be76363f
Matt Brennan <mattyb1000@gmail.com>
2014-11-12 21:46:06 +0000

    conditionals

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ function declare(name, val) {
 }
 
 function compile {
-	['=', name, val]         => declare(name, val),
-	[':=', name, val]        => b.assignmentExpression('=', compile(name), compile(val)),
-	['quote', ...rest]       => b.arrayExpression(rest.map(b.literal)),
-	[callee,  ...rest]       => b.callExpression(compile(callee), rest.map(compile)),
-	x if x instanceof String => b.literal(String(x)),
-	x                        => b.identifier(x)
+	['=', name, val]          => declare(name, val),
+	[':=', name, val]         => b.assignmentExpression('=', compile(name), compile(val)),
+	['cond', cond, then, els] => b.conditionalExpression(compile(cond), compile(then), compile(els)),
+	['quote', ...rest]        => b.arrayExpression(rest.map(b.literal)),
+	'#t'                      => b.literal(true),
+	[callee,  ...rest]        => b.callExpression(compile(callee), rest.map(compile)),
+	x if x instanceof String  => b.literal(String(x)),
+	x                         => b.identifier(x)
 }
 
-console.log(esc.generate(compile(sex("(:= a 5)"))));
+console.log(esc.generate(compile(sex("(cond #t 1 2)"))));
