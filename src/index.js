41b944b17a3fdf75ea025a4964545124645ce06a
Matt Brennan <mattyb1000@gmail.com>
2014-11-12 21:43:04 +0000

    assignment

--- src/index.js
+++ src/index.js
@@ -15,10 +15,11 @@ function declare(name, val) {
 
 function compile {
 	['=', name, val]         => declare(name, val),
+	[':=', name, val]        => b.assignmentExpression('=', compile(name), compile(val)),
 	['quote', ...rest]       => b.arrayExpression(rest.map(b.literal)),
 	[callee,  ...rest]       => b.callExpression(compile(callee), rest.map(compile)),
 	x if x instanceof String => b.literal(String(x)),
 	x                        => b.identifier(x)
 }
 
-console.log(esc.generate(compile(sex("(= a 5)"))));
+console.log(esc.generate(compile(sex("(:= a 5)"))));
