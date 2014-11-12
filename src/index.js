1c8f0db6ebe59030d5a62f61c5874ba12289b010
Matt Brennan <mattyb1000@gmail.com>
2014-11-12 22:01:40 +0000

    running stuff

--- src/index.js
+++ src/index.js
@@ -19,17 +19,33 @@ function dot(computed) {
 	};
 }
 
+function binary(op) {
+	return function (l, r) {
+		return b.binaryExpression(op, l, compile(r));
+	};
+}
+
+function fn(args, body) {
+	return b.functionExpression(
+		null, args,
+		b.blockStatement([b.returnStatement(body)])
+	)
+}
+
 function compile {
 	['=', name, val]          => declare(name, val),
 	[':=', name, val]         => b.assignmentExpression('=', compile(name), compile(val)),
 	['cond', cond, then, els] => b.conditionalExpression(compile(cond), compile(then), compile(els)),
-	['quote', ...rest]        => b.arrayExpression(rest.map(b.literal)),
+	['quote', ...rest]        => rest,
 	['.', obj, ...rest]       => rest.reduce(dot(false), compile(obj)),
 	['@', obj, ...rest]       => rest.reduce(dot(true), compile(obj)),
+	['list', ...rest]         => b.arrayExpression(rest.map(compile)),
+	['λ', args, body]         => fn(compile(args).map(b.identifier), compile(body)),
+	['+', l, ...rest]            => rest.reduce(binary('+'), compile(l)),
 	'#t'                      => b.literal(true),
 	[callee,  ...rest]        => b.callExpression(compile(callee), rest.map(compile)),
 	x if x instanceof String  => b.literal(String(x)),
 	x                         => b.identifier(x)
 }
 
-console.log(esc.generate(compile(sex("(@ a b c)"))));
+eval(esc.generate(compile(sex("((. console log) ((. (list 1 2 3) map) (λ '(a) (+ a 5))))"))));
