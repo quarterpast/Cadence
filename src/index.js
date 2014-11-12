276658151b54be01eef72a722e6d6b6fa5d9ba97
Matt Brennan <mattyb1000@gmail.com>
2014-11-12 21:52:22 +0000

    member access

--- src/index.js
+++ src/index.js
@@ -13,15 +13,23 @@ function declare(name, val) {
 	);
 }
 
+function dot(computed) {
+	return function (obj, prop) {
+		return b.memberExpression(obj, compile(prop), computed);
+	};
+}
+
 function compile {
 	['=', name, val]          => declare(name, val),
 	[':=', name, val]         => b.assignmentExpression('=', compile(name), compile(val)),
 	['cond', cond, then, els] => b.conditionalExpression(compile(cond), compile(then), compile(els)),
 	['quote', ...rest]        => b.arrayExpression(rest.map(b.literal)),
+	['.', obj, ...rest]       => rest.reduce(dot(false), compile(obj)),
+	['@', obj, ...rest]       => rest.reduce(dot(true), compile(obj)),
 	'#t'                      => b.literal(true),
 	[callee,  ...rest]        => b.callExpression(compile(callee), rest.map(compile)),
 	x if x instanceof String  => b.literal(String(x)),
 	x                         => b.identifier(x)
 }
 
-console.log(esc.generate(compile(sex("(cond #t 1 2)"))));
+console.log(esc.generate(compile(sex("(@ a b c)"))));
