777a94ef33801cde6573cc62b4b9d0f84eb51966
Matt Brennan <mattyb1000@gmail.com>
2014-11-12 14:15:27 +0000

    support string literals

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var b   = ast.builders;
 function compile(tree) {
 	return Array.isArray(tree) ? tree[0] === 'quote' ? b.arrayExpression(tree.slice(1).map(b.literal))
 	                                                 : b.callExpression(compile(tree[0]), tree.slice(1).map(compile))
-	                           : b.identifier(tree);
+	     : tree instanceof String ? b.literal(String(tree))
+	     :                     b.identifier(tree);
 }
 
-console.log(esc.generate(compile(sex('\'(+ 5 6)'))));
+console.log(esc.generate(compile(sex("(a b '(c d) \"frsnt\")"))));
