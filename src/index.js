6e1e6fc8cbdc0a935b5618bf2dcbef0f554296ec
Matt Brennan <mattyb1000@gmail.com>
2014-11-13 20:32:14 +0000

    factor quoteishes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 var b = require('ast-types').builders;
 
+function quote(fn) {
+	return function {
+		[a]    => fn(a),
+		[...a] => fn(a)
+	};
+}
+
+function id {
+	a => a
+}
+
 function quasi {
-	['unquote', ...rest] => compile(rest),
+	['unquote', ...rest] => quote(compile)(rest),
 	x => x
 }
 
 function jsLiteral {
 	x if x instanceof String => String(x),
-	['unquote', rest]        => compile(rest),
-	['unquote', ...rest]     => compile(rest),
+	['unquote', ...rest]     => quote(compile)(rest),
 	['js!', ...rest]         => jsLiteral(rest),
 	[callee, ...rest]        => b[callee].apply(b, rest.map(jsLiteral)),
 	x => x
@@ -16,8 +26,7 @@ function jsLiteral {
 
 function compile {
 	e @ Error                => { throw e; },
-	['quote', arg]           => arg,
-	['quote', ...rest]       => rest,
+	['quote', ...rest]       => quote(id)(rest),
 	['quasiquote', ...rest]  => quasi(rest),
 	['js!', ...rest]         => jsLiteral(rest),
 	[callee,  ...rest]       => b.callExpression(compile(callee), rest.map(compile)),
